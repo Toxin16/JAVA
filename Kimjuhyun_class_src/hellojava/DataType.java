c79387b653c8e86361b395ed7edaa72706bae78d
Toxin16 <109052012+Toxin16@users.noreply.github.com>
2022-09-01 12:45:58 +0900

    Add files via upload

---
+++ Kimjuhyun_class_src/hellojava/DataType.java
@@ -0,0 +1,25 @@
+package hellojava;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b1 = 1;
+		short s1 = 1;
+		int i = 1;
+		long l = 1;
+		float f = 1.0f;
+		double d = 1.0;
+		boolean b = true;
+		char c = 'A';
+		System.out.println(b1);
+		System.out.println(s1);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
---
+++ Kimjuhyun_class_src/hellojava/HelloJava.java
@@ -0,0 +1,24 @@
+package hellojava;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		//primitive data type
+		byte bt = 127;			//memory 1byte
+		short st = 32767;		//memery 2byte
+		int i = 2147483647;		//memery 4byte
+		long l = 21474836+47;	//memery 4byte
+		float f = 1.0f;			//memery 4byte
+		double d = 1.0;			//memery 8byte
+		boolean b = true;		//memery 1byte
+		char c = 'A';			//memery 2byte
+		
+		System.out.println(bt);
+		System.out.println(st);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
